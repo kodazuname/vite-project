c37fbae760dec9ab898cef56eafd23e838268d6b
233806487+kodazuname@users.noreply.github.com <kodazuname@gmail.com>
2026-07-09 11:23:59 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center text-6xl font-black text-violet-700 tracking-widest">knl</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/no</code> and save to test <code>HMR</code>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react' //stricktly check for mistakes
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
