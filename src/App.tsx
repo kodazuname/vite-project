5e57a012bf0a289b3c340d31fc40bd9739a99a54
233806487+kodazuname@users.noreply.github.com <kodazuname@gmail.com>
2026-07-09 11:32:22 +0500

    clear
    clear

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ function App() {
   return (
     <>
       <h1 className="text-center text-6xl font-black text-violet-700 tracking-widest">VITE</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/no</code> and save to test <code>HMR</code>
-        </p>
-      </div>
     </>
   )
 }
